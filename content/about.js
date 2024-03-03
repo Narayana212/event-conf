6282559c9b567cae8758ba952871271abf7b6cb4
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-03 16:15:15 +0530

    Test2 about.js

--- content/about.js
+++ content/about.js
@@ -1,4 +1,4 @@
-export const about = `Test: The annual SUMMIT Conference brings together undergraduate students
+export const about = `The annual SUMMIT Conference brings together undergraduate students
  from diverse academic backgrounds for a dynamic exchange of ideas and innovation. Through 
  a series of interactive workshops, keynote presentations, and collaborative sessions, participants 
  are encouraged to explore the intersection of their multidisciplinary studies. The conference 
@@ -22,15 +22,17 @@ institutions in the country and the world. With low faculty-student
 ratios, their in-depth attention to students lead to high levels of
 student success.`;
 
-export const aboutTheme = `The Shiv Nadar Institution of Eminence is a multidisciplinary,
-student-centric research university established in 2011 by Shiv Nadar,
-one of India&apos;s foremost philanthropists and a pioneer of
-technological revolution. It has four Schools, viz., Engineering,
-Natural Sciences, Humanities and Social Sciences, Management &
-Entrepreneurship, and the Academy of Continuing Education. We were the
-first university in the country to offer a 4-year multidisciplinary
-research degree. Today, we are continuing as trailblazers with new
-kinds of curriculum, championed by faculty drawn from some of the top
-institutions in the country and the world. With low faculty-student
-ratios, their in-depth attention to students lead to high levels of
-student success.`;
+export const aboutTheme = `At the SUMMIT Conference, each year is marked 
+by a thematic exploration that guides the discussions, workshops, and presentations
+throughout the event. Themes range from sustainability and technology to social 
+justice and healthcare, reflecting the diverse interests and passions of the 
+participating students. By centering discussions around these themes, the 
+conference aims to foster deeper insights and interdisciplinary collaboration
+among attendees. Themes serve as a catalyst for sparking innovative ideas and
+solutions to complex real-world challenges, encouraging students to think 
+critically and creatively across disciplines. Whether it's exploring the 
+role of artificial intelligence in shaping the future or addressing global
+inequality through sustainable development, the themes provide a cohesive 
+framework for advancing knowledge and driving positive change. Each year,
+the conference organizers carefully select themes that resonate with current
+global issues and inspire meaningful dialogue and action among participants.`;
