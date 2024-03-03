2e79fe6ef91e9ac36bbb53119e88f898959498a5
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-03 16:03:26 +0530

    Update about.js

--- content/about.js
+++ content/about.js
@@ -1,50 +1,13 @@
-export const about = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-dolorem atque ad nemo, non delectus. Harum, iure tempora deleniti
-vel nisi corrupti. Mollitia accusamus quas temporibus quibusdam
-natus voluptas consequuntur. Lorem ipsum dolor sit amet consectetur
-adipisicing elit. Nihil deserunt alias officia architecto, quaerat
-rerum tenetur eum voluptatum reprehenderit officiis quod aspernatur
-iusto incidunt debitis, laboriosam saepe ex cum? Excepturi. Lorem
-ipsum dolor sit amet consectetur adipisicing elit. Ratione cumque
-consectetur beatae quidem odio voluptatem doloremque autem empora
-magnam porro maiores rem eum vel aspernatur ipsam, reiciendis
-voluptate officiis voluptates. Lorem ipsum dolor sit amet,
-consectetur adipisicing elit. Pariatur esse ducimus vel porro
-impedit quidem minus repellendus optio, obcaecati modi mollitia
-cumque recusandae quibusdam. Exercitationem illum nisi adipisci
-consequuntur eligendi! Dolorem dolore, illo laudantium officia ut
-provident expedita autem similique amet numquam excepturi qui
-ducimus quas impedit libero tempore? Dolore corporis nesciunt magni
-repellendus ullam officia unde repellat deleniti nam. Ut molestias,
-vitae autem repudiandae, harum qui saepe beatae cumque nam fugiat
-explicabo delectus tempora voluptates ducimus quidem animi
-consectetur ipsam accusantium assumenda non aliquid corporis libero!
-Repellat, provident totam? Rem, fuga! Quam unde natus deleniti sit
-magni sed sequi quibusdam delectus hic harum. Eos a libero,
-voluptatem deserunt repellat necessitatibus totam quas nobis
-voluptate veritatis omnis, quibusdam amet minus? Labore magnam,
-laborum odio deserunt possimus, fuga mollitia eum non ab accusamus
-repellendus vero illum eveniet vel sint beatae necessitatibus dolore
-esse cum maxime autem? Totam repudiandae consequuntur asperiores
-earum. Modi aperiam aliquam maxime suscipit cupiditate mollitia?
-Necessitatibus aliquam suscipit perspiciatis, harum laborum, at
-inventore dolor possimus veniam dolorem nisi laudantium sint
-temporibus illo similique eveniet. Nostrum maxime ab asperiores.
-Ipsum quisquam sed amet unde. Molestias soluta temporibus nulla
-earum beatae dolorem iure asperiores tempore provident fuga, illum
-magnam veritatis similique? Quo ratione eum temporibus hic ea vitae
-totam tempore! Asperiores dolores dignissimos in nesciunt quidem,
-quaerat itaque iste corrupti quae accusantium sed? Fuga, officia
-aliquam sint nostrum, maiores qui placeat veniam ullam amet
-excepturi neque saepe, aliquid quas molestias? Reprehenderit error
-aperiam odio, soluta veniam veritatis iure asperiores ab labore at
-reiciendis ullam quo excepturi quis, omnis perspiciatis doloribus
-quam deserunt vel, ratione earum modi! Commodi distinctio
-necessitatibus nemo. Dolorum nemo, odio sunt eius nulla
-necessitatibus incidunt architecto hic! Libero corporis officia
-assumenda accusantium temporibus doloremque neque asperiores nemo
-laudantium architecto, sint, aliquid quibusdam ducimus sed. Saepe,
-eveniet similique?`;
+export const about = `Test: The annual SUMMIT Conference brings together undergraduate students
+ from diverse academic backgrounds for a dynamic exchange of ideas and innovation. Through 
+ a series of interactive workshops, keynote presentations, and collaborative sessions, participants 
+ are encouraged to explore the intersection of their multidisciplinary studies. The conference 
+ serves as a platform for fostering creativity, critical thinking, and problem-solving skills 
+ among tomorrow's leaders. With a focus on nurturing talent and fostering a culture of 
+ innovation, SUMMIT provides a unique opportunity for students to showcase their research, 
+ projects, and initiatives. Attendees leave inspired and empowered to drive positive change 
+ in their respective fields, armed with new perspectives and a network of like-minded individuals
+ dedicated to advancing knowledge and driving innovation forward.`;
 
 export const aboutUni = `The Shiv Nadar Institution of Eminence is a multidisciplinary,
 student-centric research university established in 2011 by Shiv Nadar,
