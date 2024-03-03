2e244671404d8a6bcfb0eab06b9060fc4270f315
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-03 16:22:05 +0530

    Update home.js

--- content/home.js
+++ content/home.js
@@ -1,10 +1,10 @@
-export const title=" INTERNATIONAL CONFERENCE ON ELECTRICAL, ELECTRONICS AND COMPUTING TECHNOLOGIES (ICEECT-2024) (RECORD NO. #61758) HYBRID MODE"
+export const title="SNIoE Undergraduate Multidisciplinary Mastery, Innovation and Talent"
 
 
-export const date="29th - 31th March 2024"
+export const date="20th April 2024"
 
 
-export const org="Department of Electrical Electronics and Communication Engineering, Sharda University, Greater Noida 201310"
+export const org="Office of Dean Academics, Shiv Nadar Institution of Eminence, Delhi-NCR 201314"
 
 
 
