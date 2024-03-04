dd2ece237303430a4610db309a7f4d1493083993
narayanaVivahSahyog <dev.narayana@vivahsahyog.com>
2024-03-04 23:16:19 +0530

    final changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,15 +8,16 @@ import {date, org, title} from "../../content/home"
 export default function Hero() {
   return (
     <div className=" h-[60vh] md:h-[70vh]  w-screen hero-back">
-      <MaxWidthWrapper className="pt-5 w-screen flex gap-y-5 flex-col items-start ">
-        <h1 className="text-[#CD1739]  w-[100%] md:w-[50%] font-semibold md:text-2xl text-base">
+      <MaxWidthWrapper className="pt-5 w-screen flex gap-x-5  items-start ">
+        <div className="w-[100%] md:w-[50%] mt-5 rounded-md bg-white p-5 bg-opacity-65">
+        <h1 className="text-[#CD1739]  m font-semibold md:text-2xl text-base">
          {title}
         </h1>
 
-        <p className=" text-lg md:text-3xl font-bold w-[100%] md:w-[50%]">
+        <p className=" text-lg md:text-3xl   font-bold w-[100%] md:w-[50%]">
           {date}
         </p>
-        <div className="flex items-center gap-x-5 w-[100%] md:w-[50%]">
+        <div className="flex items-center gap-x-5 ">
           <Image alt="logo" src="/logo.svg" width={150} height={150} />
           <div className="flex flex-col gap-y-3">
             <h3 className="font-semibold     text-sm md:text-lg">ORGANISED BY:</h3>
@@ -35,6 +36,10 @@ export default function Hero() {
         >
           Paper Submission
         </Link>
+        </div>
+        <div className="hidden md:block">
+         <Image alt="logo" src='/finallogo.png' className="" width={"500"} height="500"/>
+        </div>
       </MaxWidthWrapper>
     </div>
   );
