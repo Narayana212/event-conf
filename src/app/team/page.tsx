d8a3f41ab3cdd62d70a2d3f646899473fe980fb4
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-05 03:35:36 +0530

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -8,7 +8,10 @@ export default function Team() {
         <section className=" flex flex-col  items-center justify-center space-y-3">
           <h1 className="text-lg md:text-2xl text-[#CD1739]">OUR TEAM</h1>
 
-          <p>Team Members</p>
+          <p>Team Members:<br>Student Chair: Aneesh Kandrakota<br>Website Developer: Narayana Ravi
+          <br>Curation Leads: Joella Jose, Devansh Asthana<br>Design Leads: Aryaman Sen, Mahitha Prasanna<br>
+          Content Leads: Suditi Sundaram, Manasvi Vedanta<br>Marketing & PR Leads: Noor Walia, Sanskriti Garg
+          <br>Curation Panelists: Rudraksh Jawade, Sreelakshmi, Diwakar G, Malaika Dara</p>
         </section>
       </main>
     </MaxWidthWrapper>
