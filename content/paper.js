98b4bfd36ec071963adffdf2399432529db369ea
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-04 22:25:48 +0530

    Update paper.js

--- content/paper.js
+++ content/paper.js
@@ -1,21 +1,22 @@
 const conferenceGuidelines = [
-    { id: 1, point: "At least one author of an accepted paper must register and present his / her paper in the conference." },
-    { id: 2, point: "Prospective authors are invited to submit paper(s) within 6 pages written in A4 size covered under normal registration." },
-    { id: 3, point: " Extra page charges apply beyond 6 pages IEEE paper template." },
+    { id: 1, point: "Students are invited to submit papers within 6 pages written in A4 size." },
+    { id: 2, point: "At least one author of an accepted paper must present their paper in the conference." },
+    { id: 3, point: "Presenting author must be an undergraduate student currently studying at their respective institution." },
+    { id: 4, point: "Any form of plagiarism or use of generative AI is strictly banned and will be dealt with zero tolerance." },
   ];
   
 
   
   const eligibility = [
-    { id: 1, point: "The manuscript should be prepared in IEEE format using MS WORD and/or LaTeX template." },
-    { id: 2, point: "The figures/graphs/plots in the manuscript MUST be of good resolution (600 dpi or more), tables MUST NOT be in pictorial format." },
+    { id: 1, point: "The paper can be prepared in any standard format such as MLA, APA, Chicago, IEEE etc. A sample IEEE template is provided below." },
+    { id: 2, point: "The figures/graphs/plots in the paper MUST be of good resolution (600 dpi or more), tables MUST NOT be in pictorial format." },
     { id: 3, point: "Text in figures should not be too small, and preferably of equal size as text of the article." },
     { id: 4, point: "Figures, text, or the tables MUST be visible within the boundary of the text area of the page and MUST NOT go beyond it." },
     { id: 5, point: "Do NOT use any Social/Academic titles (e.g. Mr., Ms., Dr., Prof. etc) preceding the author names." },
     { id: 6, point: "Do NOT mention the position of a person (e.g., research scholar, student, assistant professor, professor, IEEE member, IEEE student member etc.) in the affiliation." },
-    { id: 7, point: "Mention full address, affiliation, and email of ALL authors, specify a corresponding author with the corresponding e-mail ID. [Once an article is accepted, the publisher will send the proofreading of the article to this e-mail]." },
+    { id: 7, point: "Mention full address, affiliation, and email of ALL authors, specify a corresponding author with the corresponding e-mail ID." },
     { id: 8, point: "Articles must be written in spell-checked and grammatically correct English." },
-    { id: 9, point: "All references, figures, and tables should be numbered in sequence starting from 1 and MUST be duly cited / referred within the text." },
+    { id: 9, point: "All references, figures, and tables MUST be duly cited/referred within the text." },
   ];
   
   
