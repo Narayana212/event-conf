7a543ea7e2107141e20351eaec43b44e14ff7c68
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-04 20:29:01 +0530

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { ThemeAcc } from "@/components/theme-acc";
 
 export default function Team() {
+  
+
+export default function Theme() {
   return (
-    <div>Team</div>
-  )
+    <MaxWidthWrapper className="pt-5 pb-5 w-screen ">
+      <main className=" gap-5 w-full md:flex-row">
+        <section className=" flex flex-col  items-center justify-center space-y-3">
+          <h1 className="text-lg md:text-2xl text-[#CD1739]">OUR TEAM</h1>
+
+          <p>"Team"</p>
+        </section>
+      </main>
+
+      <ThemeAcc />
+    </MaxWidthWrapper>
+  );
 }
