13195246d9da40d435fca9468bd99f83c2d26f39
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-05 11:23:38 +0530

    Update tracks.js

--- content/tracks.js
+++ content/tracks.js
@@ -1,35 +1,35 @@
 export const trackData = [
     {
       title: "Track 1: Sustainability and Green Future",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sapiente pariatur molestias rerum non odit nesciunt eum, beatae mollitia blanditiis possimus sed ea recusandae neque dolorum in cupiditate eos commodi.",
+      content: "Topics: Environmental Science, Green/Renewable Energy, Climate Economics, Green Solvent, Green Humanities, Eco Criticism",
     },
     {
       title: "Track 2: Advanced Materials, Manufacturing and Infrastructure",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur error sit necessitatibus nisi nihil commodi eaque, illo eum vitae! Quae, minima? Error molestiae saepe blanditiis porro eaque. Ratione, neque provident.",
+      content: "Topics: Polymers, Material Science, Composites, Nano Materials, Modelling Simulation, Additive and Digital Manufacturing, Industry 5.0, Disaster Management, Urban Network, Diagnostic Sensing",
     },
     {
       title: "Track 3: Health and Society",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Health Economics, Pharmaceutical Chemistry, Biophysics, Bioengineering, Mathematical Biology, Cancer Biology, Infectious Diseases, Genomics, Evolution, Neuro and Cognitive Sciences",
     },
     {
       title: "Track 4: Future Technologies and Innovation",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Control Systems, Robotics and Mechatronics, Nano Technology, Signal Processing and AI ML, Computer Vision, Information Security and Blockchain, Communications and Networking, Digitisation in Finance, Optoelectronic Materials",
     },
     {
       title: "Track 5: Culture and History",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Global History, Public History, Public Archaeology, History as Fiction, Narrativising History, Fashioning of the Public Sphere and Print Culture",
     },
     {
       title: "Track 6: Global Perspectives and Dynamics",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Business Models, Consumer Behaviour, Intellectual Property Rights, Macroeconomic Policy and Monetary Economics, Mathematical Finance, Social Movements and Collective Action, Globalization and Transnationalism, Social Inequality and Stratification, Conflict Resolution and Peacebuilding, Diplomacy and Foreign Policy",
     },
     {
       title: "Track 7: Energy and Interactions",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Microfluidics, Fluid-structure interaction, Multiphase flows, Energy Storage, Catalysis, Quantum Physics, Theoretical Physics, Astro Physics",
     },
     {
       title: "Track 8: 21st Century Humanity",
-      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
+      content: "Topics: Entrepreneurship, Complex Systems, Applied Algebra, Mathematical Data Sciences, Econometrics, Social Media, Gender and Queerness, Fake News",
     },
     
   ];
