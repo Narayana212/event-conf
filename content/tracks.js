da69c905ce99b3bc84418f1de15c55e98b17918c
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-05 10:17:04 +0530

    Update tracks.js

--- content/tracks.js
+++ content/tracks.js
@@ -1,34 +1,34 @@
 export const trackData = [
     {
-      title: "Track1",
+      title: "Track 1: Sustainability and Green Future",
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sapiente pariatur molestias rerum non odit nesciunt eum, beatae mollitia blanditiis possimus sed ea recusandae neque dolorum in cupiditate eos commodi.",
     },
     {
-      title: "Track2",
+      title: "Track 2: Advanced Materials, Manufacturing and Infrastructure",
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur error sit necessitatibus nisi nihil commodi eaque, illo eum vitae! Quae, minima? Error molestiae saepe blanditiis porro eaque. Ratione, neque provident.",
     },
     {
-      title: "Track3",
+      title: "Track 3: Health and Society",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     {
-      title: "Track4",
+      title: "Track 4: Future Technologies and Innovation",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     {
-      title: "Track5",
+      title: "Track 5: Culture and History",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     {
-      title: "Track6",
+      title: "Track 6: Global Perspectives and Dynamics",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     {
-      title: "Track7",
+      title: "Track 7: Energy and Interactions",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     {
-      title: "Track8",
+      title: "Track 8: 21st Century Humanity",
       content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, temporibus obcaecati assumenda ratione amet eos cupiditate, perferendis aspernatur illo eaque ut, voluptatibus quasi aliquam non reiciendis facilis sed. Facilis, veniam!",
     },
     
