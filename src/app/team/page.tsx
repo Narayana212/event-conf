fbf2119aacd08966975440667baf3669c213572a
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2024-03-05 09:23:54 +0530

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -6,12 +6,12 @@ export default function Team() {
     <MaxWidthWrapper className="pt-5 pb-5 w-screen">
       <main className="gap-5 w-full md:flex-row">
         <section className="flex flex-col items-center justify-center space-y-3">
-          <h1 className="text-2xl text-red-600 font-bold">OUR TEAM</h1>
+          <h1 className="md;text-4xl text-2xl text-[#CD1739] font-bold">OUR TEAM</h1>
 
-          <p className="text-center text-base leading-relaxed">
+          <p className=" text-base leading-relaxed">
             Team Members:
             <br />Student Chair: Aneesh Kandrakota
-            <br />Website Developer: Narayana Ravi
+            <br />Website Developer: Narayana Raavi
             <br />Curation Leads: Joella Jose, Devansh Asthana
             <br />Design Leads: Aryaman Sen, Mahitha Prasanna
             <br />Content Leads: Suditi Sundaram, Manasvi Vedanta
