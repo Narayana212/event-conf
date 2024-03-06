8fe43d5e224ba694eed89fceefb21c38d34daafc
narayanaVivahSahyog <dev.narayana@vivahsahyog.com>
2024-03-06 13:29:36 +0530

    final changes

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 
 export default function Team() {
   return (
     <MaxWidthWrapper className="pt-8 pb-8 w-screen">
       <main className="flex flex-col items-center gap-8 w-full md:flex-row">
-        <section className="flex flex-col items-center justify-center space-y-4">
-          <h1 className="text-5xl md:text-3xl text-[#CD1739] font-semibold mb-4">OUR TEAM</h1>
+        <section className="flex flex-col w-screen items-center justify-center space-y-4">
+          <h1 className="text-5xl md:text-3xl  text-[#CD1739] font-semibold mb-4">
+            OUR TEAM
+          </h1>
 
           <div className="text-lg leading-relaxed">
             <p className="mb-2">
               <strong>Convenor:</strong> Dr Partha Chatterjee
             </p>
             <p className="mb-2">
-              <strong>Co-Convenors:</strong> Dr Shabana Mitra, Dr Ankur Mehra, Dr Ajit Kumar, Dr Balamurugan Balusamy
+              <strong>Co-Convenors:</strong> Dr Shabana Mitra, Dr Ankur Mehra,
+              Dr Ajit Kumar, Dr Balamurugan Balusamy
             </p>
             <p className="mb-2">
               <strong>Student Chair:</strong> Aneesh Kandrakota
@@ -34,7 +37,8 @@ export default function Team() {
               <strong>Marketing & PR Leads:</strong> Noor Walia, Sanskriti Garg
             </p>
             <p className="mb-2">
-              <strong>Curation Panelists:</strong> Rudraksh Jawade, Sreelakshmi, Diwakar G, Malaika Dara
+              <strong>Curation Panelists:</strong> Rudraksh Jawade, Sreelakshmi,
+              Diwakar G, Malaika Dara
             </p>
           </div>
         </section>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,26 +8,29 @@ import {date, org, title} from "../../content/home"
 export default function Hero() {
   return (
     <div className=" h-[60vh] md:h-[70vh]  w-screen hero-back">
-      <MaxWidthWrapper className="pt-5 w-screen flex gap-x-5  items-start ">
-        <div className="w-[100%] md:w-[50%] mt-5 rounded-md bg-white p-5 bg-opacity-65">
-        <h1 className="text-[#CD1739]  m font-semibold md:text-2xl text-base">
+      <MaxWidthWrapper className="pt-10 w-screen flex flex-col gap-y-5  justify-center items-center ">
+        <div className="flex items-center gap-x-5  divide-x-2 divide-red-500">
+          
+          <Image alt="logo" src="/finallogo.png" width='150' height={"150"}/>
+          <div className="flex flex-col gap-y-3 pl-10">
+            <h3 className="font-semibold   text-sm ">ORGANISED BY:</h3>
+            <Image alt="univer" src='/logo.svg' width="200" height="200" />
+          </div>
+         
+          
+
+        </div>
+
+        <h1 className="text-[#CD1739] mt-7 text-center font-semibold md:text-[2rem] text-xl">
          {title}
         </h1>
 
-        <p className=" text-lg md:text-3xl   font-bold w-[100%] md:w-[50%]">
+        <p className=" font-bold ">
           {date}
         </p>
-        <div className="flex items-center gap-x-5 ">
-          <Image alt="logo" src="/logo.svg" width={150} height={150} />
-          <div className="flex flex-col gap-y-3">
-            <h3 className="font-semibold     text-sm md:text-lg">ORGANISED BY:</h3>
-            <p>
-              {org}
-            </p>
-          </div>
-        </div>
-
-        <Link
+<div className="items-center flex gap-x-5">
+  
+<Link
           href="/paper"
           className={buttonVariants({
             variant: "default",
@@ -36,10 +39,14 @@ export default function Hero() {
         >
           Paper Submission
         </Link>
-        </div>
-        <div className="hidden md:block">
-         <Image alt="logo" src='/finallogo.png' className="" width={"500"} height="500"/>
-        </div>
+
+
+       
+
+
+</div>
+
+      
       </MaxWidthWrapper>
     </div>
   );
