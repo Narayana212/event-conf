92caca177a454da8e9127b3aab6648fa01968b42
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-10 21:51:43 +0530

    Update paper.js

--- content/paper.js
+++ content/paper.js
@@ -1,8 +1,8 @@
 const conferenceGuidelines = [
-    { id: 1, point: "Students are invited to submit papers within 6 pages written in A4 size." },
+    { id: 1, point: "Students are invited to submit papers within 8000 words written in A4 size." },
     { id: 2, point: "At least one author of an accepted paper must present their paper in the conference." },
-    { id: 3, point: "Presenting author must be an undergraduate student currently studying at their respective institution." },
-    { id: 4, point: "Any form of plagiarism or use of generative AI is strictly banned and will be dealt with zero tolerance." },
+    { id: 3, point: "Presenting authors must be active undergraduate students." },
+    { id: 4, point: "Any form of plagiarism or use of generative AI is strictly prohibited and will not be tolerated." },
   ];
   
 
@@ -12,9 +12,9 @@ const conferenceGuidelines = [
     { id: 2, point: "The figures/graphs/plots in the paper MUST be of good resolution (600 dpi or more), tables MUST NOT be in pictorial format." },
     { id: 3, point: "Text in figures should not be too small, and preferably of equal size as text of the article." },
     { id: 4, point: "Figures, text, or the tables MUST be visible within the boundary of the text area of the page and MUST NOT go beyond it." },
-    { id: 5, point: "Do NOT use any Social/Academic titles (e.g. Mr., Ms., Dr., Prof. etc) preceding the author names." },
-    { id: 6, point: "Do NOT mention the position of a person (e.g., research scholar, student, assistant professor, professor, IEEE member, IEEE student member etc.) in the affiliation." },
-    { id: 7, point: "Mention full address, affiliation, and email of ALL authors, specify a corresponding author with the corresponding e-mail ID." },
+    { id: 5, point: "Do not use any social/academic titles (e.g. Mr., Ms., Dr., Prof. etc) preceding the author names." },
+    { id: 6, point: "Do <b>not</b> mention the position of a person (e.g., research scholar, student, assistant professor, professor, IEEE member, IEEE student member etc.) in the affiliation." },
+    { id: 7, point: "Mention full address, affiliation, and email of all authors." },
     { id: 8, point: "Articles must be written in spell-checked and grammatically correct English." },
     { id: 9, point: "All references, figures, and tables MUST be duly cited/referred within the text." },
   ];
