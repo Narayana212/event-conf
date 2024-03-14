b6fc527292288060141ddda610813e9fbfba8282
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-14 14:44:00 +0530

    Update home.js

--- content/home.js
+++ content/home.js
@@ -1,7 +1,7 @@
-export const title="SNIoE Undergraduate Multidisciplinary Mastery, Innovation and Talent"
+export const title="SNIoE Undergraduate Meeting on Multidisciplinary Innovation and Talent"
 
 
-export const date="20th April 2024"
+export const date="11th May 2024"
 
 
 export const org="Office of Dean Academics, Shiv Nadar Institution of Eminence, Delhi-NCR 201314"
