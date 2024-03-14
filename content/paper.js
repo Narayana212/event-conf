103a4596ac6329a498e0833bd9468910432545f8
aneekan <69073591+aneekan@users.noreply.github.com>
2024-03-14 14:47:32 +0530

    Update paper.js

--- content/paper.js
+++ content/paper.js
@@ -2,25 +2,25 @@ const conferenceGuidelines = [
     { id: 1, point: "Students are invited to submit papers within 8000 words written in A4 size." },
     { id: 2, point: "At least one author of an accepted paper must present their paper in the conference." },
     { id: 3, point: "Presenting authors must be active undergraduate students." },
-    { id: 4, point: "Any form of plagiarism or use of generative AI is strictly prohibited and will not be tolerated." },
+    { id: 4, point: "Any form of plagiarism or use of generative AI is <strong>strictly prohibited</strong> and will not be tolerated." },
   ];
   
 
   
   const eligibility = [
-    { id: 1, point: "The paper can be prepared in any standard format such as MLA, APA, Chicago, IEEE etc. A sample IEEE template is provided below." },
-    { id: 2, point: "The figures/graphs/plots in the paper MUST be of good resolution (600 dpi or more), tables MUST NOT be in pictorial format." },
+    { id: 1, point: "The paper can be prepared in any standard format such as MLA, APA, Chicago, IEEE* etc." },
+    { id: 2, point: "The figures/graphs/plots in the paper must be of good resolution (600 dpi or more), tables must not be in pictorial format." },
     { id: 3, point: "Text in figures should not be too small, and preferably of equal size as text of the article." },
-    { id: 4, point: "Figures, text, or the tables MUST be visible within the boundary of the text area of the page and MUST NOT go beyond it." },
-    { id: 5, point: "Do not use any social/academic titles (e.g. Mr., Ms., Dr., Prof. etc) preceding the author names." },
-    { id: 6, point: "Do <strong>not</strong> mention the position of a person (e.g., research scholar, student, assistant professor, professor, IEEE member, IEEE student member etc.) in the affiliation." },
+    { id: 4, point: "Figures, text, or the tables must be visible within the boundary of the text area of the page and must not go beyond it." },
+    { id: 5, point: "Do not use any social/academic titles*  preceding the author names." },
+    { id: 6, point: "Do not mention the position of a person* in the affiliation." },
     { id: 7, point: "Mention full address, affiliation, and email of all authors." },
-    { id: 8, point: "All references, figures, and tables MUST be duly cited/referred within the text." },
+    { id: 8, point: "All references, figures, and tables must be duly cited/referred within the text." },
   ];
 
 
 
- const imp="*lorem abdjhajdf kasbfjwnfnfa  fjsf fj j fj ajfncnjjk i idskjj "
+ const imp="*A sample IEEE template is provided below.<br>*(e.g. Mr., Ms., Dr., Prof. etc)\n(e.g., research scholar, student, assistant professor, professor, IEEE member, IEEE student member etc.)"
 
   
   // Exporting the arrays
